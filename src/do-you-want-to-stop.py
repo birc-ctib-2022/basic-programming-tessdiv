39dc813dbec489cb64708a3824e7782da19f024e
tessdiv <tessadivita@gmail.com>
2022-09-09 08:47:58 +0200

    added space

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -3,7 +3,16 @@
 # the answer is "yes" (lower case).
 n= input("Do you want to stop? ") #asks the initial question
 while n!='yes': #if n is not equal to yes, it will ask the question again 
-   n= input("Do you want to stop?")
+   n= input("Do you want to stop? ")
 while n =='yes': #if n is yes, it will stop
     break
+ 
+#ALSO WORKS
+#n= input("Do you want to stop? ") #asks the initial question
+#answer= input("Do you want to stop?")
+#while answer != "yes":
+#   answer= input("Do you want to stop?")
 
+#ALSO WORKS 
+#while input("Do you want to stop?") != "yes":
+#    pass 
