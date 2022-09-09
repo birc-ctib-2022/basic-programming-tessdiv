2c9bb5c811d0ddcc8c359622245ae77724397f03
tessdiv <tessadivita@gmail.com>
2022-09-09 08:45:08 +0200

    added other options and examples as comments from class

--- src/hello-n.py
+++ src/hello-n.py
@@ -6,3 +6,13 @@ n = int(input("How many times should I loop? "))
 # Now, print "Hello, World!" n times 
 for i in range (n): 
     print("Hello, World!")
+#also correct= for i in range (0,n,1): 
+
+#(0,n,1) means go from 0 to n with steps by 1 
+
+#j=0
+#while j <n:
+#    print("Hello, World!")
+#    j+=1 
+
+#NOTE IN COMPUTATIONAL WORK USE I.J.K instead of X.Y.Z
