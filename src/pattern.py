6ab03d037354a2020e9966f56745675f8bb331a3
tessdiv <tessadivita@gmail.com>
2022-12-01 14:15:53 +0100

    pattern

--- src/pattern.py
+++ src/pattern.py
@@ -9,10 +9,20 @@
 #    print (*J)
 
 #ALSO WORKS
-T= []
-for i in range (5):
-    T.append(str("*"))
-    print (" ".join(T))
-for i in range(5):
-    T.pop ()
-    print(" ".join(T))
+#T= []
+#for i in range (5):
+#    T.append(str("*"))
+#    print (" ".join(T))
+#for i in range(5):
+#    T.pop ()
+#    print(" ".join(T))
+
+stars = []
+
+for i in range(9):
+    if i <= 4:
+        stars.append('*')
+        print(*stars)
+    elif i > 4:
+        stars.pop()
+        print(*stars)
